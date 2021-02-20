0e80b66b45d0f507ba1040dc3a2a0c94549f0450
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 00:07:59 -0800

    added instructions

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,32 @@ import { Client } from "pg";
 export default (client: Client): Application =>{
     const app = express();
 
-    
+    app.post('/signup',(req, res)=>{
+        let username = req.body.username;
+        let password = req.body.password;
+        let location = req.body.location;
+        /* TODO:
+        1. Check that both username and password are not null
+        2. use Bcrypt to hash password
+        3. Parse location possibly
+        4. store in database
+        5. return 202 if succeeded, 500 if failed 
+        */
+    });
+    app.post('/signin',(req, res)=>{
+        let username = req.body.username;
+        let password = req.body.password;
+         /* TODO:
+        1. Check that both username and password are not null
+        2. use Bcrypt to hash password
+        3. Check password matches
+        4. return 202 if succeeded, 500 if failed 
+
+        */
+    });
+
+    app.post('/deleteaccount',(req, res)=>{
+        let username = req.body.username;
+    });
     return app;
 }
