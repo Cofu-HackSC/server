d113308e8f5b2266456330352ac049111e49a4dc
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 12:48:25 -0800

    search done

--- src/routes/feed.ts
+++ src/routes/feed.ts
@@ -4,13 +4,32 @@ import { Client } from "pg";
 export default (client: Client): Application => {
   const app: Application = express();
   app.get("/foods", (req, res) => {
-    client.query("SELECT * FROM Items INNER JOIN Users ON Users.id = Items.sellerid", (dbErr, dbRes) => {
-      if (dbErr) {
-        res.status(500).send(dbErr);
-      } else {
-        res.status(200).send(dbRes.rows);
+    client.query(
+      "SELECT * FROM Items INNER JOIN Users ON Users.id = Items.sellerid",
+      (dbErr, dbRes) => {
+        if (dbErr) {
+          res.status(500).send(dbErr);
+        } else {
+          res.status(200).send(dbRes.rows);
+        }
       }
-    });
+    );
   });
+
+  app.get("/search", (req, res) => {
+    let search: string = req.body;
+    client.query(
+      "SELECT * FROM Items INNER JOIN Users ON Users.id = Items.sellerid WHERE $1 IN (Items.name, Items.description, Items.ingredients)",
+      [search],
+      (dbErr, dbRes) => {
+        if (dbErr) {
+          res.status(500).send(dbErr);
+        } else {
+          res.status(200).send(dbRes.rows);
+        }
+      }
+    );
+  });
+
   return app;
 };
