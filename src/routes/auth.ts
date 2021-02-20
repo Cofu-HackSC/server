19a51d3ff28638ed5025750586932b3ca46eb4dd
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-02-20 11:43:47 -0800

    changes with Marks

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,35 @@
 import express, { Application } from "express";
 import { Client } from "pg";
+const bcrypt = require('bcrypt');
+const fetch = require('node-fetch');
 
 export default (client: Client): Application =>{
     const app = express();
 
     app.post('/signup',(req, res)=>{
+
         let username = req.body.username;
         let password = req.body.password;
         let location = req.body.location;
+        console.log('test')
+        // console.log(location)
+        if (username != null && password != null && location != null) {
+            console.log("HEY")
+            bcrypt.hash(password, 10, function(err: string, hash: string) {
+                res.send(JSON.stringify({password: hash, username: username, location:location}))
+                console.log(JSON.stringify({password: hash, username: username}))
+                // fetch('https://jsonplaceholder.typicode.com/todos', {
+                //     method: 'POST',
+                //     body: JSON.stringify({location: location, password: hash, username: username}),
+                //     headers: { 'Content-Type': 'application/json' }
+                // }).then(res => res.json())
+                //     .then(json => console.log(json));
+                // Store hash in your password DB.
+            });
+            // return code
+        } else {
+            res.send("Username or password was empty.")
+        }
         /* TODO:
         1. Check that both username and password are not null
         2. use Bcrypt to hash password
