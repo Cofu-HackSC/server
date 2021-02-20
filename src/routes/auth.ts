43244b40fecfee2d6e7dbb25de48ef280e9de88b
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 13:09:44 -0800

    auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -20,7 +20,7 @@ export default (client: Client): Application => {
           res.status(500).send("HASH ERROR");
         } else {
           client.query(
-            "INSERT INTO Users (username, password, bio, location, address, name) VALUES ($1, $2, ST_GeomFromText('POINT($3 $4))', $5, $6) RETURNING id",
+            "INSERT INTO Users (username, password, bio, location, address, name) VALUES ($1, $2, ST_GeomFromText('POINT($3 $4)'), $5, $6) RETURNING id",
             [username, hash, bio, lat, long, address],
             (dbErr, dbRes) => {
               if (dbErr) {
