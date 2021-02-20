bdc6aaa2f519b27dbe2041b559e567625196a854
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 01:24:20 -0800

    interface with redis

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,22 @@ dotenv.config();
 import express from "express";
 import { Client } from "pg";
 
+import session from "express-session";
+
+import RedisStoreUninitialized from "connect-redis";
+
+import cookieParser from "cookie-parser";
+import * as redis from "redis";
+
+const redisClient = redis.createClient(6379, process.env.REDISHOST);
+const RedisStore = RedisStoreUninitialized(session);
+
 import debug from "./routes/debug";
 import auth from "./routes/auth";
 
 const app = express();
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -15,6 +27,23 @@ const port = 8080;
 let start = async () => {
   const client = new Client();
   await client.connect();
+
+  const hour = 60 * 60 * 1000;
+  const day = hour * 24;
+  const delay = day * 40;
+
+  app.use(
+    session({
+      secret: "asddd!gds3ag.a4sga,eg2ewg",
+      store: new RedisStore({
+        client: redisClient,
+      }),
+      resave: true,
+      cookie: {
+        maxAge: Date.now() + delay,
+      },
+    })
+  );
 
   app.use("/", debug());
 
@@ -28,3 +57,9 @@ let start = async () => {
 };
 
 start();
+
+declare module 'express-session' {
+  interface Session {
+    id: string
+  }
+}
