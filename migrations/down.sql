57ac66ff3d7819ee2a17ef9143a62ce9a72d0d00
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-19 23:16:23 -0800

    database structure written

---
+++ migrations/down.sql
@@ -0,0 +1,23 @@
+-- Tables
+
+DROP TABLE CookApplication;
+
+DROP TABLE CookProfiles;
+
+DROP TABLE Sales;
+
+DROP TABLE Tags;
+
+DROP TABLE Items;
+
+DROP TABLE Users;
+
+-- Extensions
+
+DROP EXTENSION "uuid-ossp";
+
+DROP EXTENSION postgis;
+
+DROP EXTENSION address_standardizer;
+
+DROP EXTENSION address_standardizer_data_us;
---
+++ migrations/up.sql
@@ -0,0 +1,69 @@
+  
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS address_standardizer;
+CREATE EXTENSION IF NOT EXISTS address_standardizer_data_us;
+
+CREATE TABLE Users (
+    id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    bio TEXT NOT NULL,
+    photoUrl TEXT,
+    location geography(POINT)
+    address TEXT,
+    isCook BOOLEAN,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    hidePurchases BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE Items (
+    id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
+    sellerID UUID REFERENCES Users (id),
+    cost NUMERIC CHECK (cost > 0),
+    name TEXT NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
+    photoUrl TEXT,
+    -- TODO: REPLACE WITH AVG
+    averageItemRating NUMERIC,
+    ingredients TEXT NOT NULL,
+    description TEXT NOT NULL,
+    delivery BOOLEAN,
+    pickup BOOLEAN
+);
+
+CREATE TABLE Tags (
+    itemID UUID NOT NULL,
+    tag TEXT NOT NULL,
+    PRIMARY KEY(itemID, tag)
+);
+
+CREATE TABLE Sales (
+    id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
+    -- REPETITIVE - DELETE LATER - HERE NOW FOR EASE OF JOINS
+    sellerID UUID REFERENCES Users (id),
+    buyerID UUID REFERENCES Users (id),
+    itemID UUID REFERENCES Items (id),
+    itemAmount INTEGER NOT NULL DEFAULT 1,
+    totalCost NUMERIC NOT NULL CHECK (totalCost > 0),
+    -- TODO: CHECK BETWEEN 0 AND 5
+    rating NUMERIC,
+    ratingText TEXT,
+    hide BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE CookProfiles (
+    id UUID NOT NULL PRIMARY KEY UNIQUE REFERENCES Users (id),
+    -- TODO: REPLACE WITH AVG
+    averageSaleRating NUMERIC,
+    insuranceProofURI TEXT,
+    cottageFoodLicenseURI TEXT,
+    deliveryDistance NUMBER
+);
+
+CREATE TABLE CookApplication (
+    id UUID NOT NULL PRIMARY KEY UNIQUE REFERENCES Users (id),
+    insuranceProofURI TEXT,
+    cottageFoodLicenseURI TEXT,
+    approved BOOLEAN
+);
