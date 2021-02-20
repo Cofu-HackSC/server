be56e5acb811d5282d8d81ca137cd06bb01f3890
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-19 23:32:22 -0800

    Cofu express setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('We are working nodemon!')
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to Cofu🍴");
+});
+
+app.get("/status", (_req, res) => {
+    res.send("Operational🌟 " + Date());
+  });
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Cofu running at http://0.0.0.0:${port}` + Date());
+});
