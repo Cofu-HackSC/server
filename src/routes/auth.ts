89f0cdd9f4b731f38dc117339856bc4f561ea53d
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 12:18:43 -0800

    sign up done

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { Client } from "pg";
 import bcrypt from "bcrypt";
+import e from "express";
 
 export default (client: Client): Application => {
   const app = express();
@@ -10,24 +11,30 @@ export default (client: Client): Application => {
     let password: string = req.body.password ?? "";
     let bio: string = req.body.bio ?? "";
     let location: [number, number] = req.body.location;
-    let address: string = req.body.address ?? '';
+    let address: string = req.body.address ?? "";
     if (password.length > 5) {
       bcrypt.hash(password, 10, function (err, hash) {
         if (err) {
-          res.status(500).send('HASH ERROR');
+          res.status(500).send("HASH ERROR");
         } else {
-            client.query('INSERT INTO Users (username, password, bio, location, address) VALUES ($1, $2, $3, $4) RETURNING id', [username, hash, bio, location, address], (dbErr, dbRes)=>{
-                if(dbErr){
-                    res.status(500).send(dbErr.toString())
-                }else{
-                    req.session.userID = dbRes.rows[0]['id']
-                    res.status(200).send('succesfully signed up!')
-                }
-            })
+          client.query(
+            "INSERT INTO Users (username, password, bio, location, address) VALUES ($1, $2, $3, $4) RETURNING id",
+            [username, hash, bio, location, address],
+            (dbErr, dbRes) => {
+              if (dbErr) {
+                res.status(500).send(dbErr.toString());
+              } else {
+                req.session.userID = dbRes.rows[0]["id"];
+                res.status(200).send("succesfully signed up!");
+              }
+            }
+          );
         }
       });
-    }else {
-        res.status(422).send('Password too short -  must be greater than 5 characters')
+    } else {
+      res
+        .status(422)
+        .send("Password too short -  must be greater than 5 characters");
     }
     /* TODO:
         1. Check that both username and password are not null
@@ -38,15 +45,30 @@ export default (client: Client): Application => {
         */
   });
   app.post("/signin", (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    /* TODO:
-        1. Check that both username and password are not null
-        2. use Bcrypt to hash password
-        3. Check password matches
-        4. return 202 if succeeded, 500 if failed 
-
-        */
+    let username: string = req.body.username;
+    let password: string = req.body.password;
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        res.status(500).send("HASH ERROR");
+      } else {
+        client.query(
+          "SELECT id FROM Users WHERE username = $1 AND password = $2",
+          [username, hash],
+          (dbErr, dbRes) => {
+            if (dbErr) {
+              res.status(500).send(dbErr);
+            } else {
+              if (dbRes.rowCount == 0) {
+                res.status(401).send("Authentication Failed");
+              } else {
+                req.session.userID = dbRes.rows[0].id;
+                res.status(200).send("Logged In successfully");
+              }
+            }
+          }
+        );
+      }
+    });
   });
 
   app.post("/deleteaccount", (req, res) => {
