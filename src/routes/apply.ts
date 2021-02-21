c96b78352781e23433c7d4483513233771035724
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-20 17:22:03 -0800

    oops

--- src/routes/apply.ts
+++ src/routes/apply.ts
@@ -17,39 +17,43 @@ export default (client: Client): Application => {
   const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET as string);
 
   app.post("/application", upload.single("file"), (req, res) => {
-    if (!req.session.userID) {
-      res.sendStatus(401);
-      return;
-    }
-    if (!req.file) {
-      res.status(400).send("No file uploaded.");
-      return;
-    }
+    try {
+      if (!req.session.userID) {
+        res.sendStatus(401);
+        return;
+      }
+      if (!req.file) {
+        res.status(400).send("No file uploaded.");
+        return;
+      }
 
-    // Create a new blob in the bucket and upload the file data.
-    const blob = bucket.file("licenses/" + req.session.userID + ".jpeg");
-    const blobStream = blob.createWriteStream();
-    blobStream.on("error", (err) => {
-      res.status(501).send(err);
-    });
+      // Create a new blob in the bucket and upload the file data.
+      const blob = bucket.file("licenses/" + req.session.userID + ".jpeg");
+      const blobStream = blob.createWriteStream();
+      blobStream.on("error", (err) => {
+        res.status(501).send(err);
+      });
 
-    blobStream.on("finish", () => {
-      // The public URL can be used to directly access the file via HTTP.
-      const publicUrl = format(
-        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-      );
-      client.query(
-        "INSERT INTO CookApplications (id, cottageFoodLicenseURI, approved) VALUES ($1, $2, $3)",
-        [req.session.userID, publicUrl, true],
-        (dbErr, dbRes) => {
-          if (dbErr) {
-            res.status(501).send(dbErr);
-          } else {
-            res.status(200).send(publicUrl);
+      blobStream.on("finish", () => {
+        // The public URL can be used to directly access the file via HTTP.
+        const publicUrl = format(
+          `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+        );
+        client.query(
+          "INSERT INTO CookApplications (id, cottageFoodLicenseURI, approved) VALUES ($1, $2, $3)",
+          [req.session.userID, publicUrl, true],
+          (dbErr, dbRes) => {
+            if (dbErr) {
+              res.status(501).send(dbErr);
+            } else {
+              res.status(200).send(publicUrl);
+            }
           }
-        }
-      );
-    });
+        );
+      });
+    } catch (error) {
+      res.status(499).send(error);
+    }
   });
 
   return app;
